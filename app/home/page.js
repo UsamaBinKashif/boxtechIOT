297d377b8eaa79c628aa664be5c41ee98d71dfa1
Usama Ahmed <usamabinkashif.98@gmail.com>
2024-12-18 01:16:22 +0500

    latest

---
+++ app/home/page.js
@@ -0,0 +1,16 @@
+import HomePage from "./index";
+
+export const metadata = {
+  title: "Home - || Box Tech",
+  description: "Box Tech",
+};
+
+const HomePageLayout = () => {
+  return (
+    <>
+      <HomePage />
+    </>
+  );
+};
+
+export default HomePageLayout;
